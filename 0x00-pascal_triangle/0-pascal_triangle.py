116ff53fb1ce09ca20fd0f0b6cbf8fc7d2bcc481
muthee <muthee@localhost.localdomain>
2024-07-26 13:40:23 +0300

    fixed the pycode style

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,8 +2,10 @@
 """
 Pascal's Triangle Module
 
-This module contains the function `pascal_triangle` that generates Pascal's Triangle up to a given number of rows.
+This module contains the function `pascal_triangle` that generates Pascal's
+Triangle up to a given number of rows.
 """
+
 
 def pascal_triangle(n):
     """
@@ -13,17 +15,17 @@ def pascal_triangle(n):
         n (int): The number of rows of Pascal's Triangle to generate.
 
     Returns:
-        list of lists of int: Pascal's Triangle with n rows. Each row is represented as a list of integers.
+        list of lists of int: Pascal's Triangle with n rows.
+        Each row is represented as a list of integers.
     """
     if n <= 0:
         return []
 
-    triangle = [[1]] 
+    triangle = [[1]]
 
     for i in range(1, n):
-        
-        row = [1] + [triangle[i-1][j-1] + triangle[i-1][j] for j in range(1, i)] + [1]
+        row = [1] + [triangle[i - 1][j - 1] + triangle[i - 1][j]
+                     for j in range(1, i)] + [1]
         triangle.append(row)
 
     return triangle
-
